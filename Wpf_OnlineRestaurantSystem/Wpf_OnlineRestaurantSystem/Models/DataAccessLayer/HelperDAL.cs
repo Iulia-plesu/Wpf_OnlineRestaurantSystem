2b1ebeb7c1bce61a84868f0ae6478db4ddb90420
iuliap710 <111219535+iuliap710@users.noreply.github.com>
2025-05-08 22:06:14 +0300

    I connected the database

---
+++ Wpf_OnlineRestaurantSystem/Wpf_OnlineRestaurantSystem/Models/DataAccessLayer/HelperDAL.cs
@@ -0,0 +1,18 @@
+﻿using System.Configuration;
+using Microsoft.Data.SqlClient;
+
+namespace Wpf_OnlineRestaurantSystem.Models
+{
+    class HelperDAL
+    {
+        private static readonly string connectionString = ConfigurationManager.ConnectionStrings["myConStr"].ConnectionString;
+
+        public static SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(connectionString);
+            }
+        }
+    }
+}
---
+++ Wpf_OnlineRestaurantSystem/Wpf_OnlineRestaurantSystem/Models/DataAccessLayer/MenuItemDAL.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Microsoft.Data.SqlClient;
+using Wpf_OnlineRestaurantSystem.Models;
+
+namespace Wpf_OnlineRestaurantSystem.Models
+{
+    public class MenuItemDAL
+    {
+        public static List<MenuItem> GetAllMenuItems()
+        {
+            List<MenuItem> menuItems = new List<MenuItem>();
+
+            using (SqlConnection con = HelperDAL.Connection)
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("SELECT Id, Name, Price FROM MenuItems", con);
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    menuItems.Add(new MenuItem
+                    {
+                        Id = reader.GetInt32(0),
+                        Name = reader.GetString(1),
+                        Price = reader.GetDecimal(2)
+                    });
+                }
+            }
+
+            return menuItems;
+        }
+    }
+}
