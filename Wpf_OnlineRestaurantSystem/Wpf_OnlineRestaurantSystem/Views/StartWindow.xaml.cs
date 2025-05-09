36d60e7f2cd40abab07dd7b70a7ffda92054fce9
iuliap710 <111219535+iuliap710@users.noreply.github.com>
2025-05-09 14:50:10 +0300

    I changed the first window

---
+++ Wpf_OnlineRestaurantSystem/Wpf_OnlineRestaurantSystem/Views/StartWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+namespace Wpf_OnlineRestaurantSystem.Views
+{
+    public partial class StartWindow : Window
+    {
+        public StartWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            LoginWindow loginWindow = new LoginWindow(); 
+            loginWindow.Show();
+            this.Close(); 
+        }
+    }
+}
